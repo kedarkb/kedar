77f4e2c919f93ec86c4c6e66f2f909e34a03a978
kedarkb <kedarbhosale320@gmail.com>
2024-06-18 17:35:27 +0530

    Update Demo.java

--- GithubProject/src/com/cjc/Demo.java
+++ GithubProject/src/com/cjc/Demo.java
@@ -1,8 +1,16 @@
 package com.cjc;
 
 public class Demo {
+
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello");
+
+		Demo d=new Demo();
+		d.m1();
 	}
 
 }
